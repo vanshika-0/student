c1a61766cd93272dc306cb58cac48401b46f8b5e
vanshika-0 <145856550+vanshika-0@users.noreply.github.com>
2026-07-27 12:08:55 +0530

    Import API_URL from config in Login component

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 import Navbar from "../Components/Navbar";
 import { useNavigate } from "react-router-dom";
+import { API_URL } from "../config";
 
 //login hne ke bdd verify hga email jaegi
 const Login = () => {
